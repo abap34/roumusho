791dbac7ad379e362e98e6d283ad79ff26e17939
abap34 <abap0002@gmail.com>
2024-10-03 18:19:06 +0900

    use `core.info` instead of `console.log`

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const github = require('@actions/github');
 const fs = require('fs');
 
 async function run() {
-  console.log('Running action');
+  core.info('Running action');
   try {
     const token = core.getInput('GITHUB_TOKEN');
     const issueNumber = core.getInput('ISSUE_NUMBER');
     const commentBody = core.getInput('COMMENT_BODY');
     const octokit = github.getOctokit(token);
     const context = github.context;
-    console.log('octokit status', octokit);
+    core.info('octokit status', octokit);
 
     if (commentBody.startsWith('/remind')) {
       await octokit.rest.reactions.createForIssueComment({
@@ -23,7 +23,7 @@ async function run() {
       const filename = `reminder_${issueNumber}_${timestamp}.txt`;
       fs.writeFileSync(filename, commentBody);
     } else {
-      console.log('This is not a reminder comment. Skipping...');
+      core.info('This is not a reminder comment. Skipping...');
     }
   } catch (error) {
     core.setFailed(error.message);
