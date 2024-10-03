44e9e5791fd995a9aacecde803900b62cbdfc3b1
abap34 <abap0002@gmail.com>
2024-10-03 18:12:38 +0900

    add debug

--- index.js
+++ index.js
@@ -3,24 +3,27 @@ const github = require('@actions/github');
 const fs = require('fs');
 
 async function run() {
+  console.log('Running action');
   try {
     const token = core.getInput('GITHUB_TOKEN');
     const issueNumber = core.getInput('ISSUE_NUMBER');
     const commentBody = core.getInput('COMMENT_BODY');
     const octokit = github.getOctokit(token);
     const context = github.context;
+    console.log('octokit status', octokit);
 
     if (commentBody.startsWith('/remind')) {
       await octokit.rest.reactions.createForIssueComment({
         owner: context.repo.owner,
         repo: context.repo.repo,
         issue_number: issueNumber,
-        content: '+1'
+        content: '+1',
       });
-
       const timestamp = Date.now();
       const filename = `reminder_${issueNumber}_${timestamp}.txt`;
       fs.writeFileSync(filename, commentBody);
+    } else {
+      console.log('This is not a reminder comment. Skipping...');
     }
   } catch (error) {
     core.setFailed(error.message);
